2c0a2fc5a77d897f2ea01fb0703b047e106871c0
tonizak <tonizsoliman@gmail.com>
2022-12-08 03:20:41 -0800

    betty corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-int main(){
+
+/**
+*main - Entry point
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
 	printf("Size of a char: %d byte(s)", sizeof(char));
 	printf("Size of a char: %d byte(s)", sizeof(char));
 printf("Size of a char: %d byte(s)\n", sizeof(char));
@@ -7,5 +14,5 @@ printf("Size of a int: %d byte(s)\n", sizeof(int));
 printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
-	return 0;
-			}
+	return (0);
+}			}
