dbd74c9d648a491039dc672ee349f4f982932722
tonizak <tonizsoliman@gmail.com>
2022-12-08 02:55:50 -0800

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
 int main(){
-  write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",75);
+  write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 		return 0;
 }
