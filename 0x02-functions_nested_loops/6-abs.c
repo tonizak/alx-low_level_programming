67c56f84063b294a38bfe1f6d2d5f8c38346caba
tonizak <tonizsoliman@gmail.com>
2022-12-13 07:05:01 -0800

    betty6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,5 +1,11 @@
 #include <unistd.h>
 #include "main.h"
+
+/**
+ * _abs - absolute value
+ * @x: paramater passed
+ * Return: absolute value of x
+ */
 int _abs(int x)
 {
 if (x < 0)
@@ -7,6 +13,6 @@ if (x < 0)
 x = x * -1;
 return (x);
 }
-else 
+else
 return (x);
 }
