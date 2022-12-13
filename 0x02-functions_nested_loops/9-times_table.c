48c90b2da2e98597bdc67223f38dcb4e584838a6
tonizak <tonizsoliman@gmail.com>
2022-12-13 12:13:59 -0800

    rest

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,8 @@
-nclude "main.h"
+#include "main.h"
 
 /**
- *  * times_table - Prints the 9 times table, starting with 0.
- *   */
+ * times_table - Prints the 9 times table, starting with 0.
+ */
 void times_table(void)
 {
 int num, mult, prod;
