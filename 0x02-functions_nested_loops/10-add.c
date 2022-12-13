dd5b9fa27badcccfd4d0374b3e07b4527caebe5c
tonizak <tonizsoliman@gmail.com>
2022-12-13 09:20:15 -0800

    10 11 error

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,9 +3,9 @@
  * add - adds two numbers
  * @x: 1st number
  * @y: 2nd number
- * return: returns add of 2 numbers
+ * Return: returns add of 2 numbers
  */
 int add(int x, int y)
 {
-return (x+y);
+return (x + y);
 }
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,39 +1,16 @@
 #include "main.h"
+#include "stdio.h"
 
+/**
+ * print_to_98 - prints until 98
+ * @n: start number
+ * Return: 0
+ */
 void print_to_98(int n)
 {
-if (n < 0)
-{
-n *= -1;
-for(; n <= 98; n++)
-{
-_putchar(45);
-_putchar(n%10 + '0');
-_putchar(n/10 + '0');
-_putchar(' ');
-}
-}
-else if (n <= 98 && n >= 0)
-{
-for(; n <= 98; n++)
-{
-if(n < 10)
-_putchar(n + '0');
-else if (n == 10)
-{
-_putchar(n/10 +'0');
-_putchar(n%10 + '0');
-_putchar(' ');
-}
-_putchar(n%10 + '0');
-_putchar(n/10 + '0');
-_putchar(' ');
-}
-}
-for(; n >= 98; n--)
-{
-_putchar(n%10 + '0');
-_putchar(n/10 + '0');
-_putchar(' ');
-}
+while (n < 98)
+printf("%d, ", n++);
+while (n > 98)
+printf("%d, ", n--);
+printf("98\n");
 }
