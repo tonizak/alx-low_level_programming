64846ca28c71cc1613a9e17111d3446eee7edeea
tonizak <tonizsoliman@gmail.com>
2022-12-15 06:24:00 -0800

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,15 +10,16 @@ int i;
 for (i = 1; i <= 100; i++)
 {
 if (((i % 3) == 0) && (i % 5 == 0))
-printf("FizzBuzz ");
+printf("FizzBuzz");
 else if ((i % 3) == 0)
-printf("Fizz ");
+printf("Fizz");
 else if (i % 5 == 0)
-printf("Buzz ");
-else if ( i != 100)
-printf("%d ", i);
+printf("Buzz");
+else if (i != 100)
+printf("%d", i);
 else
 printf("%d", i);
+printf(" ");
 }
 printf("\n");
 return (0);
