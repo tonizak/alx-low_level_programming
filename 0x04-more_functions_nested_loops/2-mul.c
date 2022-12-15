9cc0c1febe890cd11b6413e23b52e3f3530dc301
tonizak <tonizsoliman@gmail.com>
2022-12-15 06:46:34 -0800

    2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,9 +2,9 @@
  * mul - function that multiplies 2 parameters
  * @a: first paramater
  * @b: 2nd parameter
- * return: multiplied numbers
+ * Return: multiplied numbers
  */
 int mul(int a, int b)
 {
-return (a*b);
+return (a * b);
 }
